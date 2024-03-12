068426fa354531c9a578b691c38f2449ef2dbfb7
fredgeorge0311 <george.fred0311@gmail.com>
2024-03-12 04:06:51 -0400

    create front-end 'hello world!'

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-[#16171B]">
+      <h1 className=" text-white">💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+    </div>
+  );
+}
+
+export default App;
